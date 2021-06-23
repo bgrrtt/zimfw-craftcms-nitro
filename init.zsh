33d7347cfe722d567b3ca32fb04bdac9abeee8e5
Branden Garrett <branden.garrett@gmail.com>
2021-06-23 14:03:29 -0400

    Add 0.1.0

---
+++ init.zsh
@@ -0,0 +1,103 @@
+###############################################################################
+# Aliases and Completions
+
+() {
+
+    #
+    # Nitro (Craft CMS)
+    #
+    if (( ${+commands[nitro]} )); then
+
+        #
+        # source <(nitro completion zsh)
+
+        #
+        alias nadd="nitro add"
+        alias nalias="nitro alias"
+        alias napply="nitro apply"
+        alias nblackfire="nitro blackfire"
+        alias nbridge="nitro bridge"
+        alias nclean="nitro clean"
+        alias ncompletion="nitro completion"
+        alias ncomposer="nitro composer"
+        alias ncontainer="nitro container"
+        alias ncontext="nitro context"
+        alias ncraft="nitro craft"
+        alias ncreate="nitro create"
+        alias ndb="nitro db"
+        alias ndestroy="nitro destroy"
+        alias ndisable="nitro disable"
+        alias nedit="nitro edit"
+        alias nenable="nitro enable"
+        alias nextensions="nitro extensions"
+        alias nhelp="nitro help"
+        alias nhosts="nitro hosts"
+        alias niniset="nitro iniset"
+        alias ninit="nitro init"
+        alias nlogs="nitro logs"
+        alias nls="nitro ls"
+        alias nnpm="nitro npm"
+        alias nphp="nitro php"
+        alias nportcheck="nitro portcheck"
+        alias nqueue="nitro queue"
+        alias nremove="nitro remove"
+        alias nrestart="nitro restart"
+        alias nself-update="nitro self-update"
+        alias nshare="nitro share"
+        alias nssh="nitro ssh"
+        alias nstart="nitro start"
+        alias nstop="nitro stop"
+        alias ntrust="nitro trust"
+        alias nupdate="nitro update"
+        alias nvalidate="nitro validate"
+        alias nversion="nitro version"
+        alias nxoff="nitro xoff"
+        alias nxon="nitro xon"
+
+    fi
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
